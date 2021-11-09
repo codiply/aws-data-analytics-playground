2f065a53c3393ad65ba4f428adc385c4454a79c0
Panagiotis Katsaroumpas <codiply@gmail.com>
2021-11-09 13:10:23 +0100

    Minor changes

--- infrastructure/pipeline/stacks/common_stack.py
+++ infrastructure/pipeline/stacks/common_stack.py
@@ -7,16 +7,16 @@ from ..constructs.networking import Networking
 
 
 class CommonStack(cdk.Stack):
-    def __init__(self, scope: cdk.Construct, id: str, config: EnvironmentConfig, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, id: str, config: EnvironmentConfig, include_vpc: bool, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         self._config: EnvironmentConfig = config
 
         self._define_s3_bucket()
 
-        # networking = Networking(self, 'networking', config.for_sections(['Vpc']))
-        #
-        # self.vpc = networking.vpc
+        if include_vpc:
+            networking = Networking(self, 'networking', config.for_sections(['Vpc']))
+            self.vpc = networking.vpc
 
     def _define_s3_bucket(self):
         s3_bucket_config: benedict = self._config.section('S3Bucket')
--- infrastructure/pipeline/stages/main_stage.py
+++ infrastructure/pipeline/stages/main_stage.py
@@ -8,4 +8,8 @@ class MainStage(cdk.Stage):
     def __init__(self, scope: cdk.Construct, id: str, config: EnvironmentConfig, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        CommonStack(self, f"{config.resource_prefix}-common", config.for_sections(['S3Bucket', 'Vpc']))
+        requires_vpc = False
+
+        CommonStack(self, f"{config.resource_prefix}-common",
+                    config.for_sections(['S3Bucket', 'Vpc']),
+                    include_vpc=requires_vpc)
